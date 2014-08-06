2a7414844afa0ce1d7fd4c31929f395d81b3c096
Marta Woldańska <marta.woldanska@polidea.com>
2014-08-06 15:57:43 +0200

    Cleaning up the mess in activity

--- src/pl/polidea/coverflow/testingactivity/CoverFlowTestingActivity.java
+++ src/pl/polidea/coverflow/testingactivity/CoverFlowTestingActivity.java
@@ -26,6 +26,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 
+import pl.polidea.coverflow.AbstractCoverFlowImageAdapter;
 import pl.polidea.coverflow.CoverFlow;
 import pl.polidea.coverflow.NetworkImageAdapter;
 import pl.polidea.coverflow.R;
@@ -38,9 +39,6 @@ import pl.polidea.coverflow.ResourceImageAdapter;
 public class CoverFlowTestingActivity extends Activity {
 
     private TextView textView;
-
-    private LinkedList<Result> results;
-    private ProgressDialog progressDialog;
     /*
      * (non-Javadoc)
      * 
@@ -54,43 +52,14 @@ public class CoverFlowTestingActivity extends Activity {
         textView = (TextView) findViewById(this.getResources()
                 .getIdentifier("statusText", "id", "pl.polidea.coverflow"));
 
-        if (!NetworkUtilities.isOnline(this))
-        {
-            Toast.makeText(this, "You must go online to use application", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        progressDialog = new ProgressDialog(this)
-        {
-            @Override
-            public void onBackPressed()
-            {
-                super.onBackPressed();
-                CoverFlowTestingActivity.this.finish();
-            }
-        };
-        progressDialog.setMessage("This might take a few seconds...");
-
-        try
-        {
-            results = parseJSONResult(readStringFromInputStream(getAssets().open("faces.txt")));
-
-            EmployeeAvatarTask avatarTask = new EmployeeAvatarTask(this, results);
-            avatarTask.execute();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-//        // note resources below are taken using getIdentifier to allow importing
-//        // this library as library.
-//        final CoverFlow coverFlow1 = (CoverFlow) findViewById(this.getResources().getIdentifier("coverflow", "id",
-//                "pl.polidea.coverflow"));
-//        setupCoverFlow(coverFlow1, false, results);
-//        final CoverFlow reflectingCoverFlow = (CoverFlow) findViewById(this.getResources().getIdentifier(
-//                "coverflowReflect", "id", "pl.polidea.coverflow"));
-//        setupCoverFlow(reflectingCoverFlow, true, results);
+        // note resources below are taken using getIdentifier to allow importing
+        // this library as library.
+        final CoverFlow coverFlow1 = (CoverFlow) findViewById(this.getResources().getIdentifier("coverflow", "id",
+                "pl.polidea.coverflow"));
+        setupCoverFlow(coverFlow1, false);
+        final CoverFlow reflectingCoverFlow = (CoverFlow) findViewById(this.getResources().getIdentifier(
+                "coverflowReflect", "id", "pl.polidea.coverflow"));
+        setupCoverFlow(reflectingCoverFlow, true);
     }
 
     /**
@@ -103,25 +72,18 @@ public class CoverFlowTestingActivity extends Activity {
      */
     private void setupCoverFlow(final CoverFlow mCoverFlow, final boolean reflect) {
         BaseAdapter coverImageAdapter;
-        if (reflect) {
-            coverImageAdapter = new ReflectingImageAdapter(new ResourceImageAdapter(this));
-        } else {
-            coverImageAdapter = new ResourceImageAdapter(this);
-        }
-        mCoverFlow.setAdapter(coverImageAdapter);
-        mCoverFlow.setSelection(2, true);
-        setupListeners(mCoverFlow);
-    }
 
-    // this setup is used for Polidea avatars
-    public void setupCoverFlow(final CoverFlow mCoverFlow, final boolean reflect, LinkedList<Result> results) {
-        BaseAdapter coverImageAdapter;
-        if (reflect) {
-            coverImageAdapter = new ReflectingImageAdapter(new NetworkImageAdapter(this, results));
-        } else {
-            coverImageAdapter = new NetworkImageAdapter(this, results);
-        }
+        AbstractCoverFlowImageAdapter linkedAdapter;
 
+        // select here which adapter to use
+        linkedAdapter = new ResourceImageAdapter(this);
+//        linkedAdapter = new NetworkImageAdapter(this);
+
+        if (reflect) {
+            coverImageAdapter = new ReflectingImageAdapter(linkedAdapter);
+        } else {
+            coverImageAdapter = linkedAdapter;
+        }
         mCoverFlow.setAdapter(coverImageAdapter);
         mCoverFlow.setSelection(2, true);
         setupListeners(mCoverFlow);
@@ -152,68 +114,5 @@ public class CoverFlowTestingActivity extends Activity {
                 textView.setText("Nothing clicked!");
             }
         });
-    }
-
-    private String readStringFromInputStream(InputStream inputStream) throws IOException
-    {
-        StringBuilder builder = new StringBuilder();
-
-        int c;
-
-        while((c = inputStream.read()) != -1)
-        {
-           builder.append((char)c);
-        }
-
-        return builder.toString();
-    }
-
-    private LinkedList<Result> parseJSONResult(String result)
-    {
-        LinkedList<Result> results = new LinkedList<Result>();
-
-        try
-        {
-            JSONObject outerItem = new JSONObject(result);
-            JSONArray jArray = outerItem.getJSONArray("people");
-
-            for (int i = 0; i < jArray.length(); i++)
-            {
-                JSONObject employee = jArray.getJSONObject(i);
-                String id = employee.getString("id");
-
-                URL url = new URL(employee.getString("gravatar"));
-
-                Result employeeResult = new Result(url, id, null);
-                results.add(employeeResult);
-            }
-        }
-        catch (MalformedURLException e)
-        {
-            Log.e("CoverFlowTestingActivity", "MalformedUrlException in parseJSONResult");
-            e.printStackTrace();
-        }
-        catch (JSONException e)
-        {
-            Log.e("CoverFlowTestingActivity", "JSONException in parseJSONResult");
-            e.printStackTrace();
-        }
-
-        return results;
-    }
-
-    public LinkedList<Result> getResults()
-    {
-        return results;
-    }
-
-    public void setResults(LinkedList<Result> results)
-    {
-        this.results = results;
-    }
-
-    public ProgressDialog getProgressDialog()
-    {
-        return progressDialog;
     }
 }
