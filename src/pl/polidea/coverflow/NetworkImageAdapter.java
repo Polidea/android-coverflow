5fc2bca1637a97ef0c3c1ef05211cfd0fc649b8f
Marta Woldańska <marta.woldanska@polidea.com>
2014-08-06 15:56:34 +0200

    NetworkImageAdapter parses JSON file and shows Chuck Norris on every ImageView - work in progress

--- src/pl/polidea/coverflow/NetworkImageAdapter.java
+++ src/pl/polidea/coverflow/NetworkImageAdapter.java
@@ -3,17 +3,27 @@ package pl.polidea.coverflow;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import pl.polidea.coverflow.testingactivity.EmployeeAvatarTask;
 import pl.polidea.coverflow.testingactivity.Result;
 
 /**
@@ -23,50 +33,31 @@ import pl.polidea.coverflow.testingactivity.Result;
  * 
  */
 public class NetworkImageAdapter extends AbstractCoverFlowImageAdapter {
-
     /** The Constant TAG. */
     private static final String TAG = NetworkImageAdapter.class.getSimpleName();
-
-    /** The Constant DEFAULT_LIST_SIZE. */
-    private static final int DEFAULT_LIST_SIZE = 20;
-
-    /** The Constant IMAGE_RESOURCE_IDS. */
-    private static final List<Integer> IMAGE_RESOURCE_IDS = new ArrayList<Integer>(DEFAULT_LIST_SIZE);
-
-    /** The Constant DEFAULT_RESOURCE_LIST. */
-    private static final int[] DEFAULT_RESOURCE_LIST = { R.drawable.image01, R.drawable.image02, R.drawable.image03,
-            R.drawable.image04, R.drawable.image05 };
 
     /** The bitmap map. */
     private final Map<Integer, WeakReference<Bitmap>> bitmapMap = new HashMap<Integer, WeakReference<Bitmap>>();
 
     private final Context context;
 
-
-    private LinkedList<Result> results;
     private static final List<Uri> IMAGE_RESOURCE_URIS = new ArrayList<Uri>();
 
-    /**
-     * Creates the adapter with default set of resource images.
-     *
-     * @param context
-     *            context
-     */
-    public NetworkImageAdapter(final Context context) {
-        super();
-        this.context = context;
-        setResources(DEFAULT_RESOURCE_LIST);
-    }
-
-    public NetworkImageAdapter(final Context context, LinkedList<Result> results)
+    public NetworkImageAdapter(final Context context)
     {
         super();
+
         this.context = context;
-        this.results = results;
-        setResources();
+        try {
+            setResources(parseJSONResult(readStringFromInputStream(context.getAssets().open("faces.txt"))));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    private final synchronized void setResources()
+    public final synchronized void setResources(List<Result> results)
     {
         IMAGE_RESOURCE_URIS.clear();
 
@@ -78,20 +69,6 @@ public class NetworkImageAdapter extends AbstractCoverFlowImageAdapter {
         notifyDataSetChanged();
     }
 
-    /**
-     * Replaces resources with those specified.
-     * 
-     * @param resourceIds
-     *            array of ids of resources.
-     */
-    public final synchronized void setResources(final int[] resourceIds) {
-        IMAGE_RESOURCE_IDS.clear();
-        for (final int resourceId : resourceIds) {
-            IMAGE_RESOURCE_IDS.add(resourceId);
-        }
-        notifyDataSetChanged();
-    }
-
     /*
      * (non-Javadoc)
      * 
@@ -100,8 +77,6 @@ public class NetworkImageAdapter extends AbstractCoverFlowImageAdapter {
     @Override
     public synchronized int getCount()
     {
-        // return this size if you use standard images
-        //return IMAGE_RESOURCE_IDS.size();
         return IMAGE_RESOURCE_URIS.size();
     }
 
@@ -112,24 +87,54 @@ public class NetworkImageAdapter extends AbstractCoverFlowImageAdapter {
      */
     @Override
     protected Bitmap createBitmap(final int position) {
-        Log.v(TAG, "creating item " + position);
+        return ((BitmapDrawable) context.getResources().getDrawable(R.drawable.image01)).getBitmap();
+    }
 
-        // use this code to use standard images
-//        final Bitmap bitmap = ((BitmapDrawable) context.getResources().getDrawable(IMAGE_RESOURCE_IDS.get(position)))
-//                .getBitmap();
+    private String readStringFromInputStream(InputStream inputStream) throws IOException
+    {
+        StringBuilder builder = new StringBuilder();
 
-        final Bitmap bitmap;
+        int c;
+
+        while((c = inputStream.read()) != -1)
+        {
+            builder.append((char)c);
+        }
+
+        return builder.toString();
+    }
+
+    private LinkedList<Result> parseJSONResult(String result)
+    {
+        LinkedList<Result> results = new LinkedList<Result>();
+
         try
         {
-            bitmap = BitmapFactory.decodeStream(context.getContentResolver().openInputStream(results.get(position).getAvatarUri()));
+            JSONObject outerItem = new JSONObject(result);
+            JSONArray jArray = outerItem.getJSONArray("people");
+
+            for (int i = 0; i < jArray.length(); i++)
+            {
+                JSONObject employee = jArray.getJSONObject(i);
+                String id = employee.getString("id");
+
+                URL url = new URL(employee.getString("gravatar"));
+
+                Result employeeResult = new Result(url, id, null);
+                results.add(employeeResult);
+            }
         }
-        catch (FileNotFoundException e)
+        catch (MalformedURLException e)
         {
-            Log.e("ResourceImageAdapter", "FileNotFoundException at createBitmap");
-            return null;
+            Log.e("CoverFlowTestingActivity", "MalformedUrlException in parseJSONResult");
+            e.printStackTrace();
+        }
+        catch (JSONException e)
+        {
+            Log.e("CoverFlowTestingActivity", "JSONException in parseJSONResult");
+            e.printStackTrace();
         }
 
-        bitmapMap.put(position, new WeakReference<Bitmap>(bitmap));
-        return bitmap;
+        return results;
     }
 }
